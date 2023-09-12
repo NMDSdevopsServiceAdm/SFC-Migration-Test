0edbcf8b035704eca8430f16dbde5b2438f889cb
Sam Tripp <1519984+sbt92@users.noreply.github.com>
2023-09-12 11:24:46 +0100

    Install puppeteer dependancies

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -67,7 +67,7 @@ module.exports = function (config) {
     customLaunchers:{
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--user-data-dir'],
+        flags: ['--no-sandbox'],
       }
     },
     browserDisconnectTimeout: 10000,
